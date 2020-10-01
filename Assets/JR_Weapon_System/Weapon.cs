adeec3f5008aac66cb2f05b472e7edbbcde11df4
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-10-01 16:08:57 -0400

    Added new Manufacturer script

--- Assets/JR_Weapon_System/Weapon.cs
+++ Assets/JR_Weapon_System/Weapon.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public abstract class Weapon
 {
+    public Manufacturer manufacturer;
     public int magazineSize;
     public int damage;
     public double rof;
     public int accuracy;
     public int range;
 
+    public AnimationClip reloadAnim;
+
+    public virtual void Fire()
+    {
+
+    }
+
+    public virtual void Reload()
+    {
+
+    }
 
 }
